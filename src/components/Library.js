581949e0dfd9287319394fe19dcab69c3e7d84e3
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-02-24 21:01:11 +0330

    add cards

--- src/components/Library.js
+++ src/components/Library.js
@@ -47,13 +47,23 @@ function Library() {
           <div style={inputFieldStyle}>
             <div class="grid gap-6 mb-6 md:grid-cols-3">
               <span class="column">
-                <button class="bg-emerald-200 hover:bg-emerald-300 text-white px-8 py-1 rounded-full">
+                <button
+                  class="bg-emerald-300 hover:bg-emerald-400 text-white px-8 py-1 rounded-full"
+                  style={{
+                    width: 200,
+                  }}
+                >
                   اعمال
                 </button>
               </span>
 
               <span class="column">
-                <button class="bg-gray-300 hover:bg-gray-400 text-white px-8 py-1 rounded-full">
+                <button
+                  class="bg-emerald-300 hover:bg-emerald-400 text-white px-8 py-1 rounded-full"
+                  style={{
+                    width: 200,
+                  }}
+                >
                   فیلتر
                 </button>
               </span>
@@ -70,35 +80,91 @@ function Library() {
                     onChange={(event) => {
                       setSearch(event.target.value);
                     }}
+                    style={{
+                      width: 350,
+                    }}
                   ></input>
                 </form>
               </span>
             </div>
 
-            <div class="row">
+            <div class="grid gap-3 mb-6 md:grid-cols-4">
               <div class="max-w-sm rounded overflow-hidden shadow-lg">
                 <img
                   class="w-full"
                   src={card_img}
                   alt="Sunset in the mountains"
                 ></img>
-                <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                  <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                    exercitationem praesentium nihil.
-                  </p>
+                <div class="px-6 py-3 bg-zinc-300">
+                  <div class="font-bold text-xl mb-2">Lib Name</div>
                 </div>
-                <div class="px-6 pt-4 pb-2">
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #photography
+                <div class="px-6 bg-zinc-300">
+                  <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Type
                   </span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #travel
+                </div>
+              </div>
+
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  class="w-full"
+                  src={card_img}
+                  alt="Sunset in the mountains"
+                ></img>
+                <div class="px-6 py-3 bg-zinc-300">
+                  <div class="font-bold text-xl mb-2">Lib Name</div>
+                </div>
+                <div class="px-6 bg-zinc-300">
+                  <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Type
                   </span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #winter
+                </div>
+              </div>
+
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  class="w-full"
+                  src={card_img}
+                  alt="Sunset in the mountains"
+                ></img>
+                <div class="px-6 py-3 bg-zinc-300">
+                  <div class="font-bold text-xl mb-2">Lib Name</div>
+                </div>
+                <div class="px-6 bg-zinc-300">
+                  <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Type
+                  </span>
+                </div>
+              </div>
+
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  class="w-full"
+                  src={card_img}
+                  alt="Sunset in the mountains"
+                ></img>
+                <div class="px-6 py-3 bg-zinc-300">
+                  <div class="font-bold text-xl mb-2">Lib Name</div>
+                </div>
+                <div class="px-6 bg-zinc-300">
+                  <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Type
+                  </span>
+                </div>
+              </div>
+
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img
+                  class="w-full"
+                  src={card_img}
+                  alt="Sunset in the mountains"
+                ></img>
+                <div class="px-6 py-3 bg-zinc-300">
+                  <div class="font-bold text-xl mb-2">Lib Name</div>
+                </div>
+                <div class="px-6 bg-zinc-300">
+                  <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    Type
                   </span>
                 </div>
               </div>
