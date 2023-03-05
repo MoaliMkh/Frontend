897f64623de1828ace2f2c15d915079d43a579c9
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2023-03-05 07:18:55 +0330

    feat: add content delete

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -3,12 +3,16 @@ import card_img from "../card.jpg";
 import req from '../api/user_req'
 import { useNavigate } from 'react-router-dom';
 import { useAlert } from 'react-alert';
+import { useState } from 'react';
 
 
 const ContentList = (props) => {
 
     const navigate = useNavigate();
     const alert = useAlert();
+
+    const [libraryList, setLibraryList] = useState(props.libList)
+    const [libListChanged, setLibListChanged] = useState(false)
 
     const fetchEachContent = async (index) => {
         // const token = localStorage.getItem("token");
@@ -33,7 +37,15 @@ const ContentList = (props) => {
 
 
         try {
-          const res = await req.delete(`/${user_id}/library/${library_id}/file/${index}/`, {headers: {"Authorization": `Token ${token}`}})
+          await req.delete(`/${user_id}/library/${library_id}/file/${index}/`, {headers: {"Authorization": `Token ${token}`}})
+          
+          
+          const res = await req.get(`/${user_id}/library/${library_id}/file/`, {headers: {"Authorization": `Token ${token}`}})
+          setLibListChanged(true)
+          localStorage.setItem("each_lib", JSON.stringify(res.data))
+          setLibraryList(JSON.parse(localStorage.getItem("each_lib")))
+          setLibListChanged(false)
+          
           alert.show('محتوا با موفقیت حذف شد')
           navigate('/eachlibrary')
         } catch (e) {
@@ -47,16 +59,19 @@ const ContentList = (props) => {
     const renderContents = (library, idx) => {
     return (
 
-    <li style={{width: '80%', marginBottom: '15%'}} key={idx} onClick={() => {fetchEachContent(idx)}} >
+    <li style={{width: '80%', marginBottom: '15%'}} key={idx}  >
         <div class="max-w-sm rounded overflow-hidden shadow-lg"  >
-            <img
-                class="w-full"
-                src={card_img}
-                alt="Sunset in the mountains"
-            ></img>
-            <div class="px-6 py-3 bg-zinc-300">
-                <div class="font-bold text-xl mb-2">{library.name}</div>
+            <div onClick={() => {fetchEachContent(idx)}}>
+                <img
+                    class="w-full"
+                    src={card_img}
+                    alt="Sunset in the mountains"
+                ></img>
+                <div class="px-6 py-3 bg-zinc-300">
+                    <div class="font-bold text-xl mb-2">{library.name}</div>
+                </div>
             </div>
+
             <div class="px-6 bg-zinc-300 flex">
                 <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                 {library.content_type}
@@ -75,7 +90,7 @@ const ContentList = (props) => {
     return (
         <ul>
             <FlatList
-            list={props.libList}
+            list={libraryList}
             renderItem={renderContents}
             renderWhenEmpty={() => <div style={{color: 'red', textAlign: 'center'}}>محتوایی برای نمایش وجود ندارد</div>}
             display={{
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -2,11 +2,17 @@ import FlatList from 'flatlist-react';
 import card_img from "../card.jpg";
 import req from '../api/user_req'
 import { useNavigate } from 'react-router-dom';
+import { useAlert } from 'react-alert';
+import { useState } from 'react';
 
 
 const LibraryList = (props) => {
 
     const navigate = useNavigate();
+    const alert = useAlert();
+
+    const [libraryList, setLibraryList] = useState(props.libList)
+    const [libListChanged, setLibListChanged] = useState(false)
 
     const fetchEachLibrary = async (index) => {
         const token = localStorage.getItem("token");
@@ -29,30 +35,36 @@ const LibraryList = (props) => {
         localStorage.setItem("library_id", index)
 
         try {
-          const res = await req.get(`/${user_id}/library/${index}/file/`, {headers: {"Authorization": `Token ${token}`}})
-          localStorage.setItem("each_lib", JSON.stringify(res.data))
-          navigate('/eachlibrary')
+          await req.delete(`/${user_id}/library/${index}/`, {headers: {"Authorization": `Token ${token}`}})
+          const res = await req.get(`/${user_id}/library/`, {headers: {"Authorization": `Token ${token}`}})
+
+          setLibListChanged(true)
+          localStorage.setItem("all_libs", JSON.stringify(res.data))
+          setLibraryList(JSON.parse(localStorage.getItem("all_libs")))
+          setLibListChanged(false)
+          alert.show('کتابخانه با موفقیت حذف شد')
         } catch (e) {
           console.log(e)
         }
-
-        /////// TODO
     }
 
 
     const renderLibrary = (library, idx) => {
     return (
 
-    <li style={{width: '80%', marginBottom: '15%'}} key={idx} onClick={() => {fetchEachLibrary(idx)}} >
+    <li style={{width: '80%', marginBottom: '15%'}} key={idx}  >
         <div class="max-w-sm rounded overflow-hidden shadow-lg"  >
-            <img
-                class="w-full"
-                src={card_img}
-                alt="Sunset in the mountains"
-            ></img>
-            <div class="px-6 py-3 bg-zinc-300">
-                <div class="font-bold text-xl mb-2">{library.name}</div>
+            <div onClick={() => {fetchEachLibrary(idx)}}>
+                <img
+                    class="w-full"
+                    src={card_img}
+                    alt="Sunset in the mountains"
+                ></img>
+                <div class="px-6 py-3 bg-zinc-300">
+                    <div class="font-bold text-xl mb-2">{library.name}</div>
+                </div>
             </div>
+
             <div class="px-6 bg-zinc-300 flex">
                 <span class="inline-block bg-zinc-400 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                 {library.content_type}
@@ -71,7 +83,7 @@ const LibraryList = (props) => {
     return (
         <ul>
             <FlatList
-            list={props.libList}
+            list={libraryList}
             renderItem={renderLibrary}
             renderWhenEmpty={() => <div style={{color: 'red', textAlign: 'center'}}>کتابخانه‌ای برای نمایش وجود ندارد</div>}
             display={{
